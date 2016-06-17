9af3933275152b8df69a28c30bf327eeb169fc22
Kuro Hsu <kurotanshi@gmail.com>
2016-06-17 17:29:18 +0800

    workshop-v1

--- wrokshop2/src/App.vue
+++ wrokshop2/src/App.vue
@@ -1,6 +1,65 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <div class="panel panel-default">
+      <div class="panel-heading">Task Table</div>
+      <table class="table">
+        <!-- 表頭 -->
+        <thead>
+          <tr>
+            <th class="number">#</th>
+            <th class="name">Task Name</th>
+            <th class="duration">Duration</th>
+            <th class="status">Status</th>
+            <th class="edit"></th>
+          </tr>
+        </thead>
+        <!-- 表頭 -->
+
+        <tbody>
+          <tr>
+            <th class="number" scope="row">1</th>
+            <td>
+              <div>Name</div>
+              <input type="text">
+            </td>
+            <td class="duration">
+              <div>123</div>
+              <input type="text">
+            </td>
+            <td class="status">
+              <div>Open</div>
+              <input type="checkbox">
+            </td>
+            <td class="edit">
+              <button class="btn btn-primary">編輯</button>
+              <button class="btn btn-danger">刪除</button>
+            </td>
+          </tr>
+        </tbody>
+
+        <!-- 新增用 -->
+        <tfoot>
+          <tr>
+            <th class="number" scope="row">新增</th>
+            <td>
+              <input type="text">
+            </td>
+            <td class="duration">
+              <input type="text">
+            </td>
+            <td  class="status">
+              <div>Open?</div>
+              <input type="checkbox">
+            </td>
+            <td class="edit">
+              <button class="btn btn-success">加入</button>
+              <button class="btn btn-warning">重置</button>
+            </td>
+          </tr>
+        </tfoot>
+        <!-- 新增用 -->
+      </table>
+    </div>
   </div>
 </template>
 
@@ -14,10 +73,17 @@ export default {
 }
 </script>
 
+
 <style>
 #app{
   width: 980px; margin: 10px;
 }
+body {
+  font-family: Helvetica Neue, Arial, sans-serif;
+  font-size: 14px;
+  color: #444;
+}
+thead{
+  font-size: 16px; font-weight: 900;
+}
 </style>
-
-
